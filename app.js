33903378e615a328870950756fd61dc15a200dc3
MBA <matheus-araujo1998@hotmail.com>
2022-05-16 13:18:35 -0300

    fixed the bugs with md5 and mysql

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const https = require("https");
 const _ = require('lodash');
 const mysql = require('mysql');
+const md5 = require('md5');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -19,13 +20,6 @@ var con = mysql.createConnection({
     database: process.env.DB_BASE
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
-
-
-
 app.route('/')
     .get(function (req, res){
         res.render("index", {route: "index"});
@@ -35,8 +29,28 @@ app.route('/')
         res.redirect("/");
     });
 
+app.route('/login')
+    .post(function (req, res){
+        let username = req.body.username;
+        let password = md5(req.body.password);
+
+        const queryLogin = "SELECT * FROM usuarios WHERE login = '"+username+"' AND password = '"+password+"'";
+
+        var query = con.query(queryLogin, function(err, result) {
+            if (err) throw err;
+            if(result.length == 1){
+                let url = "/lobby/"+result[0].id_login;
+                res.redirect(url);
+            }else{
+                res.redirect("/")
+            }
+        });
+
+    });
+
+
 app.get("/lobby/:user", function(req, res){
-    const UID = _.lowerCase(req.params.user);
+    const UID = _.lowerCase(req.body.user);
 
     res.render("user", {
         route: "user",
